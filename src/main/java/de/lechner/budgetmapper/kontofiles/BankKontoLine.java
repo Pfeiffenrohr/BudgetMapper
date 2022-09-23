d47ffe8cd2c0766afa36bf71a59c55a59e00e17b
lechnerri <lechnerri@media-saturn.com>
2022-09-23 09:00:55 +0200

    angepasst

---
+++ src/main/java/de/lechner/budgetmapper/kontofiles/BankKontoLine.java
@@ -0,0 +1,40 @@
+package de.lechner.budgetmapper.kontofiles;
+
+import com.opencsv.bean.CsvBindByPosition;
+
+
+public class BankKontoLine {
+
+    @CsvBindByPosition(position = 2)
+    private String buchungstag;
+    @CsvBindByPosition(position = 4)
+    private String verwendungszweck;
+    @CsvBindByPosition(position = 14)
+    private String betrag;
+
+
+
+    public String getBuchungstag() {
+        return buchungstag;
+    }
+
+    public void setBuchungstag(String buchungstag) {
+        this.buchungstag = buchungstag;
+    }
+
+    public String getVerwendungszweck() {
+        return verwendungszweck;
+    }
+
+    public void setVerwendungszweck(String verwendungszweck) {
+        this.verwendungszweck = verwendungszweck;
+    }
+
+    public String getBetrag() {
+        return betrag;
+    }
+
+    public void setBetrag(String betrag) {
+        this.betrag = betrag;
+    }
+}
