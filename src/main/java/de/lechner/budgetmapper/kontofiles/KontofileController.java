3e4efc558fc2549e93af4b4e3f97df7665728a04
Richard Lechner <lechnerri@mediamarktsaturn.com>
2025-12-02 09:33:57 +0100

    Changes

--- src/main/java/de/lechner/budgetmapper/kontofiles/KontofileController.java
+++ src/main/java/de/lechner/budgetmapper/kontofiles/KontofileController.java
@@ -6,20 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KontofileController {
+    String fileKonto1 = "C:/temp/20250206-539619-umsatz.CSV";
+    String fileKonto2 = "C:/temp/20250206-6192967-umsatz.CSV";
 
     @Autowired
     private KontofileService kontofileService;
 
     @GetMapping(value = "/getFile")
     public String sayHi() throws Exception {
-
-
-        return kontofileService.readCsvFile("C:/temp/20250206-539619-umsatz.CSV");
+        return kontofileService.readCsvFile(fileKonto1);
     }
     @GetMapping(value = "/getFile2")
     public String giro2() throws Exception {
-
-
-        return kontofileService.readCsvFile2("C:/temp/20250206-6192967-umsatz.CSV");
+        return kontofileService.readCsvFile2(fileKonto2);
     }
 }
